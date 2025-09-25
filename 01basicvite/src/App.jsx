99971acec4e01d14219380a8cc9d6d9e9e06edd6
karthik5033 <karthikkp5033@gmail.com>
2025-09-26 00:21:56 +0530

    k

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,9 +1,10 @@
 import Youtube from "./yt";
 function App() {
+  const username="KP"
   return (
     <>
       <h1>Vite React app</h1>;
-      <h1>Vite React app</h1>;
+      <h1>Vite React app {username}</h1>;
       <Youtube/>
     </>
   );
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -2,5 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
+import { Children } from "react";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const reactElement={
+  type:'a',
+  props:{
+    href:"https:google.com",
+    target:'_blank'
+  },
+  Children:'Click me to visit google'
+}
+
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom React App</h1>
+    </div>
+  )
+}
+const AnotherElement=(
+  <a href="https://google.com" target="_blank">Visit google</a>
+)
+
+const areactElement=React.createElement(
+  'a',
+  {href:'https://google.com', target:'_blank'},
+  'Click to visit google'
+)
+
+// ReactDOM.createRoot(document.getElementById("root")).render(AnotherElement );
+// ReactDOM.createRoot(document.getElementById("root")).render(<MyApp/>);
+// ReactDOM.createRoot(document.getElementById("root")).render(areactElement);
